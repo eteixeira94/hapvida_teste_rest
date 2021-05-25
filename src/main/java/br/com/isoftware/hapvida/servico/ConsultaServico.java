7fa89e6eeb1847fd9c28161b19343902f51fa6f6
Ednardo Teixeira <ednardo994@gmail.com>
2021-05-25 16:20:44 -0300

    Implementando salvar da consulta para uso do many to many

--- src/main/java/br/com/isoftware/hapvida/servico/ConsultaServico.java
+++ src/main/java/br/com/isoftware/hapvida/servico/ConsultaServico.java
@@ -1,13 +1,35 @@
 package br.com.isoftware.hapvida.servico;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.isoftware.hapvida.entidade.Consulta;
+import br.com.isoftware.hapvida.entidade.ConsultaHasAnimal;
 import br.com.isoftware.hapvida.entidade.enums.StatusConsulta;
 import br.com.isoftware.hapvida.repositorio.ConsultaRepositorio;
 
 @Service
 public class ConsultaServico extends ServicoGenerico<Long, Consulta, ConsultaRepositorio> {
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public void salvar(Consulta e) {
+        Set<ConsultaHasAnimal> itens = e.getConsultaHasAnimals();
+        e.setConsultaHasAnimals(null);
+
+        this.doSalvar(e);
+        this.getRepositorio().save(e);
+
+        if (itens != null && !itens.isEmpty()) {
+            itens.forEach(i -> i.getId().setConsultaId(e.getId()));
+            e.setConsultaHasAnimals(itens);
+            this.getRepositorio().save(e);
+        }
+
+        this.posSalvar(e);
+    }
 
     @Override
     public void doSalvar(Consulta entidade) {
